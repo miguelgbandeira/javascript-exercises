7e98e5876a037abdfd020afae6fdac38d1b9201c
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-03-21 19:19:25 +0000

    Add solution to final exercise, 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  arr.forEach((person) => {
+    if (!("yearOfDeath" in person)) {
+      const currentDate = new Date();
+      person.yearOfDeath = currentDate.getFullYear();
+    }
+    person.age = person.yearOfDeath - person.yearOfBirth;
+  });
 
+  return arr.reduce((oldest, curr) => {
+    const oldestAge = oldest.age;
+    const currentAge = curr.age;
+    return oldestAge < currentAge ? curr : oldest;
+  });
 };
 
 // Do not edit below this line
