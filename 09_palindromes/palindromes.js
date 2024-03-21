9d04c597cecffa7ddef36819050b902fb2b789d2
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-03-21 18:31:41 +0000

    Add solutions for 9 and 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedString = cleanStr.split("").reverse().join("");
 
+  return cleanStr == reversedString;
 };
 
 // Do not edit below this line
