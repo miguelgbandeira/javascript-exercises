f315cf92a6486cf59e418bd8f6ba00407026552a
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-03-21 18:11:36 +0000

    Add solutions up until exercise 8

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, numberOfRepetitions) {
+  if (numberOfRepetitions < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < numberOfRepetitions; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  if (min < 0 || max < 0 || typeof min != "number" || typeof max != "number") {
+    return "ERROR";
+  }
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let result = 0;
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
+
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
